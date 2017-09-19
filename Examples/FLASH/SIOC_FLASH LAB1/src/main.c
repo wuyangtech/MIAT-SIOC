e9492ef35873e4508b730bd35539cba764f2db95
wuyangtech <31952893+wuyangtech@users.noreply.github.com>
2017-09-19 14:01:56 +0800

    Examples

---
+++ Examples/FLASH/SIOC_FLASH LAB1/src/main.c
@@ -0,0 +1,156 @@
+/******************** (C) COPYRIGHT 2008 STMicroelectronics ********************
+* File Name          : main.c
+* Author             : MCD Application Team
+* Version            : V2.0.3
+* Date               : 09/22/2008
+* Description        : Main program body
+********************************************************************************
+* THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+* WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE TIME.
+* AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY DIRECT,
+* INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING FROM THE
+* CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE CODING
+* INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+*******************************************************************************/
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f10x.h"
+#include "stdio.h"
+#include "hw_config.h"
+#include "platform_config.h"
+
+
+/* Private typedef -----------------------------------------------------------*/
+typedef enum {FAILED = 0, PASSED = !FAILED} TestStatus;
+/* Private define ------------------------------------------------------------*/
+
+#define StartAddr  ((u32)0x08008000)
+#define EndAddr    ((u32)0x0800C000)
+
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+
+u32 EraseCounter = 0x00, Address = 0x00;
+u32 Data;
+vu32 NbrOfPage = 0x00;
+volatile FLASH_Status FLASHStatus;
+volatile TestStatus MemoryProgramStatus;
+
+
+
+/* Private function prototypes -----------------------------------------------*/
+
+void NVIC_Configuration(void);
+void Delay(vu32 nCount);
+
+
+/* Private functions ---------------------------------------------------------*/
+/*******************************************************************************
+* Function Name  : main
+* Description    : Main program.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+int main(void)
+{
+  u32 *temp;
+
+  Set_System();
+
+  /*pause*/
+  getchar();
+
+  printf("start \r\n");
+
+ 
+  FLASHStatus = FLASH_COMPLETE;
+  MemoryProgramStatus = PASSED;
+  Data = 0x15041925;
+
+  /* NVIC configuration */
+  NVIC_Configuration();
+
+  /* Unlock the Flash Program Erase controller */
+  FLASH_Unlock();
+
+  /* Define the number of page to be erased */
+  NbrOfPage = (EndAddr - StartAddr) / FLASH_PAGE_SIZE;
+
+  /* Clear All pending flags */
+  FLASH_ClearFlag(FLASH_FLAG_BSY | FLASH_FLAG_EOP | FLASH_FLAG_PGERR | FLASH_FLAG_WRPRTERR);	
+
+  /* Erase the FLASH pages */
+  for(EraseCounter = 0; (EraseCounter < NbrOfPage) && (FLASHStatus == FLASH_COMPLETE); EraseCounter++)
+  {
+    FLASHStatus = FLASH_ErasePage(StartAddr + (FLASH_PAGE_SIZE * EraseCounter));
+  }
+  /*  FLASH Word program of data 0x15041979 at addresses defined by StartAddr and EndAddr*/
+  Address = StartAddr;
+
+  while((Address < EndAddr) && (FLASHStatus == FLASH_COMPLETE))
+  {
+    FLASHStatus = FLASH_ProgramWord(Address, Data);
+	temp=(u32 *)Address;
+	printf("0x%x data is 0x%x \r\n",Address,*temp);
+    Address = Address + 4;
+  }
+  
+  /* Check the corectness of written data */
+  
+  Address = StartAddr;
+
+  while((Address < EndAddr) && (MemoryProgramStatus != FAILED))
+  {
+    if((*(vu32*) Address) != Data)
+    {
+      MemoryProgramStatus = FAILED;
+      printf("FAILED \r\n");
+    }
+    Address += 4;
+  }
+  
+  
+
+  while(1)
+  {
+  }
+	
+ 
+}
+
+/*******************************************************************************
+* Function Name  : NVIC_Configuration
+* Description    : Configures the nested vectored interrupt controller.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void NVIC_Configuration(void)
+{
+
+
+#ifdef  VECT_TAB_RAM
+  /* Set the Vector Table base location at 0x20000000 */
+  NVIC_SetVectorTable(NVIC_VectTab_RAM, 0x0);
+#else  /* VECT_TAB_FLASH  */
+  /* Set the Vector Table base location at 0x08000000 */
+  //NVIC_SetVectorTable(NVIC_VectTab_FLASH, 0x0);
+  NVIC_SetVectorTable(0x08003000, 0x0);
+#endif
+
+
+}
+
+
+/*******************************************************************************
+* Function Name  : Delay
+* Description    : Inserts a delay time.
+* Input          : nCount: specifies the delay time length.
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void Delay(vu32 nCount)
+{
+  for(; nCount != 0; nCount--);
+}
